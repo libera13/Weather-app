9b1ab9371a1f42ea13ec9b6ab68d8415e8e53aa7
libera13 <emillibera@gmail.com>
2020-05-18 14:49:30 +0200

    Updates

---
+++ precache-manifest.75d58362a7d3ad5291a954db72582faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dbfe2f75d346150f68dbaebba2874f",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "5e6675b8650fc3898f0b",
+    "url": "/Weather-app/static/css/main.631d8bbd.chunk.css"
+  },
+  {
+    "revision": "2c2610b110b0ed5ff41e",
+    "url": "/Weather-app/static/js/2.7a44d962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.7a44d962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6675b8650fc3898f0b",
+    "url": "/Weather-app/static/js/main.aeebd48b.chunk.js"
+  },
+  {
+    "revision": "2d0a7c9edd510091e8d9",
+    "url": "/Weather-app/static/js/runtime-main.222fae0d.js"
+  },
+  {
+    "revision": "d8eae38cfb89c9772f97c6932aeac4ea",
+    "url": "/Weather-app/static/media/white-currant-3467373_1920.d8eae38c.jpg"
+  }
+]);
